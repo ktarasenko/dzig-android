9383c4b3f7fb9d1bb808e847a8d8163e2ca78a94
underwood <vpodlesnyak@gmail.com>
2012-11-15 16:00:24 +0200

    project created

---
+++ src/com/dzig/IntroActivity.java
@@ -0,0 +1,13 @@
+package com.dzig;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class IntroActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
