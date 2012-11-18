da65a0aa43364a7d24a3246b71732e65b7075d29
Konstantin Tarasenko <konstantin.v.tarasenko@gmail.com>
2012-11-18 20:58:20 +0200

    cleanup of cleanup

--- src\com\dzig\activities\IntroActivity.java
+++
@@ -1,7 +0,0 @@
-package com.dzig.activities;
-
-import android.support.v4.app.FragmentActivity;
-
-public class IntroActivity extends FragmentActivity {
-
-}
--- src\com\dzig\fragments\IntroFragment.java
+++
@@ -1,7 +0,0 @@
-package com.dzig.fragments;
-
-import android.support.v4.app.Fragment;
-
-public class IntroFragment extends Fragment{
-
-}
